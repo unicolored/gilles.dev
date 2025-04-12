9127cf6658aa1c68b2e0d15039bf9c6e92109884
Gilles Hoarau <unicolored@gmail.com>
2025-04-12 22:06:20 +0200

    Add unicoloredBaseUrl to environment configurations
    Added the `unicoloredBaseUrl` property to both production and development environment files. This ensures the application can correctly reference the Unicolored service in different environments.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
   endpoints: {
     _self: 'http://localhost:4242',
   },
+  unicoloredBaseUrl: 'http://localhost:4125',
   algolia: {
     appId: 'SUXVC6B2YE',
     apiKey: 'c6b499da6c9903652a2b4cc7a281d7b9',
