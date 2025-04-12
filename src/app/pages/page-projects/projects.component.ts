4df7410537aa1fe8ea9bb02520969b94e1c43bb2
Gilles Hoarau <unicolored@gmail.com>
2025-04-12 22:06:39 +0200

    Refactor ProjectsComponent to use signals and computed properties
    Replaced RxJS subjects with Angular signals for managing `posts`. Introduced a computed property to streamline template updates and integrated environment variables for dynamic image URLs. Simplified code for better readability and alignment with modern Angular practices.

--- src/app/pages/page-projects/projects.component.ts
+++ src/app/pages/page-projects/projects.component.ts
@@ -1,32 +1,48 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { HttpService, JekyllPost, UnicoloredService } from 'ngx-services';
-import { Subject, takeUntil, tap } from 'rxjs';
+import { tap } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-projects-page',
-  imports: [],
   template: `
-    @for (post of posts; track post.id) {
-      <p>
-        {{ post.title }}
-      </p>
+    @for (post of postsComputed(); track post.id) {
+      <div>
+        <h2>{{ post.title }}</h2>
+        <p>⭐️ {{ post.id }} // {{post.thumbnail}}</p>
+        @if (post.thumbnail) {
+<!--          <img width="100" height="100" [ngSrc]="post.thumbnail" [alt]="'IMAGE'"/>-->
+          <img width="100" height="100" [src]="environment.unicoloredBaseUrl+post.thumbnail" [alt]="'IMAGE'"/>
+        }
+<!--        <div [innerHTML]="post.excerpt">-->
+<!--        </div>-->
+      </div>
+      <hr>
+    } @empty {
+      <p>There are no items.</p>
     }
   `,
   styles: ``,
   providers: [HttpService, UnicoloredService],
+  imports: [
+    // NgOptimizedImage
+  ]
 })
 export class ProjectsComponent implements OnInit {
   unicoloredService = inject(UnicoloredService);
 
-  posts: JekyllPost[] = [];
+  posts = signal<JekyllPost[]>([]);
+  postsComputed = computed(() => {
+    return this.posts();
+  })
 
   ngOnInit() {
     this.unicoloredService
       .getAll('projects-gilles.json')
-      .pipe(tap((data) => (this.posts = data)))
-      .subscribe((data: JekyllPost[]) => {
-        this.posts = data;
-        console.log('Posts:', this.posts);
-      });
+      .pipe(tap((data) => this.posts.set( data as JekyllPost[])))
+      .subscribe();
+
   }
+
+  protected readonly environment = environment;
 }
