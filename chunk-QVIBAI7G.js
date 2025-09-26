d4cad6a25c4472b30ca871fabe408cf34a4fa84f
unicolored <unicolored@gmail.com>
2025-09-26 19:37:50 +0000

    Deploying to gh-pages from @ unicolored/gilles.dev@547c878643cdf93e05da31b1dc25f5ed2d9dd10c 🚀

--- chunk-QVIBAI7G.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-PYSQ3QI5.js";import"./chunk-BNF2EWO2.js";import"./chunk-UJILXD2X.js";import"./chunk-WM5GWSBU.js";export{a as NotFoundComponent};
--- chunk-YOVB3TP3.js
+++
@@ -1,1 +0,0 @@
-function r(n){let t=n.match(/\/([^/]+?)-\d+x\d+\.[a-z]{3,4}$/i);return t?t[1]:null}export{r as a};
