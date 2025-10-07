e87cc16bd65770aa247dbc2605ea0bb59ed1d7ff
Gilles Hoarau <unicolored@gmail.com>
2025-10-07 23:57:36 +0200

    Use href instead of routerLink

--- src/app/elements/header/header.component.ts
+++ src/app/elements/header/header.component.ts
@@ -14,7 +14,7 @@ import { CommonModule } from '@angular/common';
         @for (item of navItems; track item.name) {
           <a
             class="item"
-            [routerLink]="item.route"
+            [href]="item.route"
             [routerLinkActive]="'active'"
             [routerLinkActiveOptions]="{ exact: true }"
             [title]="item.name"
