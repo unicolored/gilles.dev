4ab944b9d9e19a5cfb308f61867c95ead9f191a9
Gilles Hoarau <unicolored@gmail.com>
2025-10-08 23:41:44 +0200

    Do not use enum

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -1,15 +1,20 @@
-export enum PageIdSlugEnum {
-  home = 'home',
-  about = 'about',
-  contact = 'contact',
-  search = 'search',
-  portfolio = 'portfolio',
-}
+export const PageIdSlugEnum = {
+  home: 'home',
+  about: 'about',
+  contact: 'contact',
+  search: 'search',
+  portfolio: 'portfolio',
+} as const;
 
-export enum PortfolioListSlug {
-  logo = 'gilles-dev-logo-design',
-  visual = 'gilles-dev-visual-identity',
-  ecommerce = 'gilles-dev-online-store',
-  development = 'gilles-dev-development',
-  threed = 'gilles-dev-3d-stuff',
-}
+export type PageIdSlugKeys = (typeof PageIdSlugEnum)[keyof typeof PageIdSlugEnum];
+
+export const PortfolioListSlug = {
+  ecommerce: 'gilles-dev-online-store',
+  development: 'gilles-dev-development',
+  logo: 'gilles-dev-logo-design',
+  threed: 'gilles-dev-3d-stuff',
+  visual: 'gilles-dev-visual-identity',
+} as const;
+
+// Convert object key in a type
+export type PortfolioListSlugKeys = (typeof PortfolioListSlug)[keyof typeof PortfolioListSlug];
