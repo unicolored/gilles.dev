3ac72efc046ee76be678e75feb915db7a106a517
Gilles Hoarau <unicolored@gmail.com>
2025-10-15 20:37:13 +0200

    Update slugs

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -12,11 +12,12 @@ export const PageIdSlugEnum = {
 export type PageIdSlugKeys = (typeof PageIdSlugEnum)[keyof typeof PageIdSlugEnum];
 
 export const PortfolioListSlug = {
-  ecommerce: 'gilles-dev-online-store',
-  development: 'gilles-dev-development',
-  logo: 'gilles-dev-logo-design',
-  threed: 'gilles-dev-3d-stuff',
-  visual: 'gilles-dev-visual-identity',
+  portfolio: 'gilles-dev-portfolio',
+  // ecommerce: 'gilles-dev-online-store',
+  // development: 'gilles-dev-development',
+  // logo: 'gilles-dev-logo-design',
+  // threed: 'gilles-dev-3d-stuff',
+  // visual: 'gilles-dev-visual-identity',
 } as const;
 
 // Convert object key in a type
