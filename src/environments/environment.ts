a19e75a9c0c26e3c89041f2628ab46bbb5973ec5
Gilles Hoarau <unicolored@gmail.com>
2025-10-23 17:04:56 +0200

    Update endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,11 @@
 export const environment = {
   maintenance: false,
   endpoints: {
-    _self: 'http://192.168.1.58:4242',
+    _self: 'http://localhost:4242',
     api: 'https://myadmin.unicolo.red/api',
     backend: 'https://localhost:9999',
-    hub: 'http://localhost:9998/.well-known/mercure',
+    //hub: 'http://localhost:9998/.well-known/mercure',
+    hub: 'https://localhost:9999/.well-known/mercure',
   },
   topic: {
     token: '!ChangeThisMercureHubJWTSecretKey!',
