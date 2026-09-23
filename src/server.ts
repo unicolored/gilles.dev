7be5c767f1373052611549e5e155a6975d76974e
Gilles Wonder <unicolored@gmail.com>
2026-09-23 12:48:51 +0200

    Add allowedHosts

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const browserDistFolder = resolve(serverDistFolder, '../browser');
 const indexHtml = join(serverDistFolder, 'index.server.html');
 
 const app = express();
-const commonEngine = new CommonEngine();
+const commonEngine = new CommonEngine({ allowedHosts: ['localhost', '*.gilles.dev'] });
 
 /**
  * Example Express Rest API endpoints can be defined here.
